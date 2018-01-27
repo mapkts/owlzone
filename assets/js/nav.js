c82257e9dda19bca4676515e13a2d5c0d3dc09d8
vilscrolls <vilscrolls@gmail.com>
2018-01-27 22:09:39 +0800

    Huge version update

---
+++ assets/js/nav.js
@@ -0,0 +1,70 @@
+(function toggleNav() {
+	var item = document.getElementById('nav-main'),
+		icon = document.getElementById('nav-icon');
+
+	icon.addEventListener('click', function() {
+		if(item.className === 'nav-main') {
+			item.className += ' nav-dropdown';
+		} else {
+			item.className = 'nav-main';
+		}
+	}, false);
+})();
+
+(function toggleSearch() {
+	var scope = document.getElementById('trigger-search'),
+		close = document.getElementById('collapse-search'),
+		bar = document.getElementById('search-field'),
+		logo = document.getElementsByClassName('logo')[0];
+
+	scope.addEventListener('click', function() {
+		this.className += ' search-hidden';
+		close.className += ' search-visible';
+		bar.className += ' search-visible';
+		logo.className += ' logo-responsive';
+	}, false);
+
+	close.addEventListener('click', function() {
+		this.className = 'search-close';
+		scope.className = 'search-scope';
+		bar.className = 'search-field';
+		logo.className = 'logo';
+	}, false);
+})();
+
+(function flexGallery() {
+	var el = document.getElementsByClassName('js-img-gallery')[0],
+		preString = "<li><a href='#'><img class='gallery-img' src='assets/img/gallery/",
+		postString = "'></a></li>";
+
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'gallery.json', true);
+	xhr.send(null);
+	xhr.onreadystatechange = function() {
+		if (xhr.readystate == 4 && xhr.status === 200) {
+			attachHtml();
+		}
+	};
+
+	function attachHtml() {
+		// if innerHTML exists, remove it for redrawing.
+		el.innerHTML = "";
+
+		// push at least 4 images.
+		for(var i = 0; i < 4; i++) {
+			el.innerHTML += preString + xhr.responseText[i] + postString;
+		}
+
+		// push additional images according to innerWidth.
+		var width = 385;
+		for(var j = 4; j < 12; j++) {
+			if (window.innerWidth > width) {
+				el.innerHTML += preString + xhr.responseText[j] + postString;
+				width += 75;
+			}
+		}
+	}
+
+	// if window has resized, invoke attachHtml.
+	window.addEventListener("resize", attachHtml);
+})();
