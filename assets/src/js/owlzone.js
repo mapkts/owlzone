caa409581bbf5aad2a83afcee34650352640f66d
vilscrolls <vilscrolls@gmail.com>
2019-03-17 20:14:35 +0800

    Update script

--- assets/src/js/owlzone.js
+++ assets/src/js/owlzone.js
@@ -109,7 +109,7 @@
 
     // Tab switching
     function switchTab() {
-      var tabs = $('.widget-area');
+      var tabs = $$('.widget-area');
       var tabCates = $('.post-category');
       var tabTags = $('.post-tags');
 
