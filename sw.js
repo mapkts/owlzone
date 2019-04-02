f7de1a8b5b7d0999853fcae95cde8c40179663ba
vilscrolls <vilscrolls@gmail.com>
2019-04-02 21:18:32 +0800

    update

--- sw.js
+++ sw.js
@@ -22,6 +22,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       supportsWebp().then(bool => isWebpSupported = bool);
+      console.log(supportsWebp(), isWebpSupported)
       urlsToCache.push('/assets/img/owl-and-rat' + (isWebpSupported ? '.webp' : '.jpg'));
       return cache.addAll(urlsToCache);
     }).then(function () {
