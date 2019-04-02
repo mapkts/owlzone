99ad5e310f0f39d291a8b62b72894e3084039d94
vilscrolls <vilscrolls@gmail.com>
2019-04-02 20:43:30 +0800

    Update

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
-var version = '2.7.9';
-var urlsToCache = [
+let isWebpSupported
+const version = '2.8.0';
+const CACHE_NAME = 'owlzone-sw-cache::v' + version;
+const urlsToCache = [
   '/',
-  '/assets/js/bundle.min.js',
-  '/assets/js/home.min.js',
   '/assets/img/code.svg',
-  '/assets/img/owl-and-rat.jpg',
-  '/assets/img/owl-and-rat.webp',
+  '/assets/js/home.min.js',
+  '/assets/js/bundle.min.js',
   '/assets/font/nuFlD-vYSZviVYUb_rj3ij__anPXBb__lW4e5g.woff2',
 ];
-var CACHE_NAME = 'owlzone-sw-cache::v' + version;
+
+async function supportsWebp() {
+  if (!self.createImageBitmap) return false;
+
+  const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+  const blob = await fetch(webpData).then(r => r.blob());
+  return createImageBitmap(blob).then(() => true, () => false);
+}
+
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-        return cache.addAll(urlsToCache);
-    }).then(function() {
+      supportsWebp().then(bool => isWebpSupported = bool);
+      urlsToCache.push('/assets/img/owl-and-rat' + isWebpSupported ? '.webp' : '.jpg');
+      return cache.addAll(urlsToCache);
+    }).then(function () {
       // Force the SW to transition from installing to active state
       return self.skipWaiting();
     })
@@ -39,13 +49,14 @@ self.addEventListener('fetch', function (event) {
   if (event.request.method !== 'GET') {
     event.respondWith(
       fetch(event.request).catch(function () {
-          return caches.match('/offline.html');
+        return caches.match('/offline.html');
       })
     );
     return;
   }
+
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
